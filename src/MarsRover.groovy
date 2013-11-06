b10cbaaeb6a871d42ddcebcf565d072c709efd05
JashaadGaines <jagaines29@live.com>
2013-11-06 14:02:44 -0600

    [greg + jashaad] initial commit

---
+++ src/MarsRover.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworker
+ * Date: 11/6/13
+ * Time: 1:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MarsRover {
+}
